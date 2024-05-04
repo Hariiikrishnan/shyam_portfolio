e2e74de0df6154f5f29d35ffb322abfcf7efa4b3
Hariiikrishnan <therihari36@gmail.com>
2024-05-05 00:36:23 +0530

    changes by shyam -1

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function closeSideBar(){
      
   },400)
  }
+
+
+// var user_name = document.getElementsByClassName("name")[0].value;
+// var mail = document.getElementsByClassName("mail")[0].value;
+// var msg = document.getElementsByClassName("msg")[0].value;
+// console.log(document.getElementsByClassName("name")[0]);
+// console.log(document.getElementsByClassName("name")[0].value);
+function sendMail(){
+  //  console.log(document.getElementsByClassName("name")[0].value);
+  //  console.log(document.getElementsByClassName("mail")[0].value);
+  //  console.log(document.getElementsByClassName("msg")[0].value);
+   var usr_name = document.getElementsByClassName("name")[0].value;
+   var mail = document.getElementsByClassName("mail")[0].value;
+  var msg = document.getElementsByClassName("msg")[0].value;
+  console.log(usr_name);
+  console.log(mail);
+  console.log(msg);
+  window.location = `mailto:shyam2lca@gmail.com?subject=From%20${usr_name}&body=${msg}`;
+ }
